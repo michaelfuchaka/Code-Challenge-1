5e7be267fee20517c791d4a4a963b004cd487fb9
michaelfuchaka <fuchakamichael06@gmail.com>
2025-06-06 23:47:45 +0300

    writing code in chai.js and modified README.md file

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-  ##Code challenge 1
+  ## Code challenge 1
 
   This repository has solutions to the following Javascript challenges
 
 1. **Chai calculator**
+
                  Calculates the basic ingredients needed to make a specific number of cups of Kenyan chai.
 
  2.  **Boda fare calculator**
+
                 Estimates the fare for a boda boda ride.
 
  3.  **Mobile money estimator**
+
                 Estimates transaction fees.
 
+
+      ## Author
+
+      Michael Fuchaka
+
+    ## License  
+
+   This code is for educational purposes only,not for commercial use! 
---
+++ chaiCalculator/chai.js
@@ -0,0 +1,35 @@
+
+function calculateChaiIngredients (){
+    
+  //Prompts the user to enter the number of chai cups they want to make
+     let chaiCups = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+
+    //  converts the input to a number.
+    let numberOfCups = Number(chaiCups);
+
+   //  Calculates the required amount for each ingredient 
+    const water = numberOfCups * 200;
+    const milk = numberOfCups * 50;
+    const tea =  numberOfCups* 1;
+    const sugar = numberOfCups* 2;
+
+  // Prints the results to the console 
+  if (typeof numberOfCups==='number' && numberOfCups > 0){
+    console.log(`
+        To make ${numberOfCups} cups of kenyan chai, you will need:
+        Water:${water} ml
+        Milk: ${milk} ml
+        Tea Leaves (Majani): ${tea} tablespoons
+        Sugar (Sukari): ${sugar} teaspoons
+
+        Enjoy your Chai Bora!
+        ` );
+     }   
+     else {console.error(alert("Invalid input!,please try again"))}
+    }
+   
+      calculateChaiIngredients();
+     
+     
+
+          
